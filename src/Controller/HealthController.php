9cb5704a8d3f398c367d4a84f567444dc59a72a2
Marek Dąbrowski <notyzop@gmail.com>
2026-02-01 16:15:15 +0100

    disable symfony autowire

--- src/Controller/HealthController.php
+++ src/Controller/HealthController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -11,8 +12,9 @@ use Symfony\Component\Routing\Attribute\Route;
 #[Route('/api/health', methods: ['GET'])]
 final class HealthController
 {
-    public function __invoke(): Response
+    public function __invoke(LoggerInterface $logger): Response
     {
+        $logger->info('checking application health');
         return new JsonResponse(['status' => 'ok']);
     }
 }
